0c2b7afc8f4de539b62a2afcffcda43283622835
akash-kumar-s0 <akashkumars@presidio.com>
2025-12-28 23:13:16 +0530

    [add] check api

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,18 +2,19 @@
 import express from "express";
 import dotenv from "dotenv";
 import checkRoute from "./routes/check.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/check", checkRoute);
 
 app.get('/health', (req, res) => {
   res.send('Healthy!! Healthy!!');
 });
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
